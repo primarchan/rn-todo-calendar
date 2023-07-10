3855ce2e187d0719eda7d188121e752c858d2e4e
An T.G <boltcarrier@naver.com>
2023-07-11 01:54:46 +0900

    FlatList 를 통한 캘린더 UI 구현

--- App.js
+++ App.js
@@ -1,20 +1,131 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { getCalendarColumns, getDayColor, getDayText } from "./src/util";
+import dayjs from "dayjs";
+import { useEffect } from "react";
+import { runPracticeDayjs } from "./src/practice-dayjs";
+import Margin from "./src/Margin";
+import { SimpleLineIcons } from "@expo/vector-icons";
+
+const columnSize = 35;
 
 export default function App() {
+  const now = dayjs();
+  const columns = getCalendarColumns(now);
+
+  const Column = ({ text, color, opacity }) => {
+    return (
+      <View
+        style={{
+          width: columnSize,
+          height: columnSize,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ color, opacity }}>{text}</Text>
+      </View>
+    );
+  };
+
+  const ListHeaderComponent = () => {
+    const currentDateText = dayjs(now).format("YYYY.MM.DD.");
+
+    const ArrowButton = ({ iconName, onPress }) => {
+      return (
+        <TouchableOpacity
+          onPress={onPress}
+          style={{ paddingHorizontal: 20, paddingVertical: 15 }}
+        >
+          <SimpleLineIcons name={iconName} size={15} color="#404040" />
+        </TouchableOpacity>
+      );
+    };
+
+    return (
+      <View>
+        {/* < YYYY.MM.DD. > */}
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <ArrowButton iconName={"arrow-left"} onPress={() => {}} />
+
+          <TouchableOpacity>
+            <Text style={{ fontSize: 20, color: "#404040" }}>
+              {currentDateText}
+            </Text>
+          </TouchableOpacity>
+
+          <ArrowButton iconName={"arrow-right"} onPress={() => {}} />
+        </View>
+        <View style={{ flexDirection: "row" }}>
+          {[0, 1, 2, 3, 4, 5, 6].map((day) => {
+            const dayText = getDayText(day);
+            const color = getDayColor(day);
+            return (
+              <Column
+                key={`day-${day}`}
+                text={dayText}
+                color={color}
+                opacity={1}
+              />
+            );
+          })}
+        </View>
+      </View>
+    );
+  };
+
+  const renderItem = ({ item: date }) => {
+    const dataText = dayjs(date).get("date");
+    const day = dayjs(date).get("day");
+    const color = getDayColor(day);
+    const isCurrentMonth = dayjs(date).isSame(now, "month");
+
+    return (
+      <Column
+        text={dataText}
+        color={color}
+        opacity={isCurrentMonth ? 1 : 0.4}
+      />
+    );
+  };
+
+  useEffect(() => {
+    runPracticeDayjs();
+
+    console.log("columns", columns);
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={columns}
+        keyExtractor={(_, index) => `column-${index}`}
+        numColumns={7}
+        ListHeaderComponent={ListHeaderComponent}
+        renderItem={renderItem}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/util.js
@@ -0,0 +1,82 @@
+import dayjs from "dayjs";
+
+export const fillEmptyColumns = (columns, start, end) => {
+  const filledColumns = columns.slice(0);
+
+  // 1. 첫날 이전 공백 채우기
+  const startDay = dayjs(start).get("day"); // 화 2
+  for (let i = 1; i <= startDay; i += 1) {
+    const date = dayjs(start).subtract(i, "day");
+    filledColumns.unshift(date);
+  }
+  // ["10.31", "11.1", ..., "11.30"]
+  // ["10.30", "10.31", "11.1", ..., "11.30"]
+
+  // 2. 마지막날 이후 공백 채우기
+  const endDay = dayjs(end).get("day");
+  /**
+    0 -> 6
+    1 -> 5
+    2 -> 4
+    endDay + ? = 6
+   */
+  // ["10.31", "11.1", ..., "11.30", "12.1"]
+  // ["10.31", "11.1", ..., "11.30", "12.1", "12.2"]
+  // ["10.31", "11.1", ..., "11.30", "12.1", "12.2", "12.3"]
+  for (let i = 1; i <= 6 - endDay; i += 1) {
+    const date = dayjs(end).add(i, "day");
+    filledColumns.push(date);
+  }
+
+  return filledColumns;
+};
+export const getCalendarColumns = (now) => {
+  const start = dayjs(now).startOf("month"); // 11.1
+  const end = dayjs(now).endOf("month"); // 11.30
+  const endDate = dayjs(end).get("date"); // 30
+
+  const columns = [];
+  for (let i = 0; i < endDate; i += 1) {
+    const date = dayjs(start).add(i, "day");
+    columns.push(date);
+  }
+
+  const filledColumns = fillEmptyColumns(columns, start, end);
+  console.log("columns 최종", columns);
+  // ["10.31", "11.1", ..., "11.30", "12.1", "12.2", "12.3"]
+  return filledColumns;
+};
+
+/**
+ * @param day 0 ~ 6
+ * @return 일 ~ 월
+ */
+export const getDayTextBySwitchCase = (day) => {
+  switch (day) {
+    case 0:
+      return "일";
+    case 1:
+      return "월";
+    case 2:
+      return "화";
+    case 3:
+      return "수";
+    case 4:
+      return "목";
+    case 5:
+      return "금";
+    case 6:
+      return "토";
+    default:
+      return "";
+  }
+};
+
+const dayTexts = ["일", "월", "화", "수", "목", "금", "토"];
+export const getDayText = (day) => {
+  return dayTexts[day];
+};
+
+export const getDayColor = (day) => {
+  return day === 0 ? "#e67639" : day === 6 ? "#5872d1" : "#2b2b2b";
+};
